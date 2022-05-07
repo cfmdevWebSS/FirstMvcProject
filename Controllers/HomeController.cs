58d69c5ccd950bd11eb9508d719f7d05491234da
Lawrence Buta <larry.buta@gmail.com>
2022-05-07 18:49:58 -0400

    FirstMvcProject-05-07-2022-V1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("https://oakacademy.uk/");
+        }
+
+        public ActionResult FirstAction()
+        {
+            return View();
+        }
+
+        public ActionResult GotoProduct()
+        {
+            //this version we list the controller first, then the action...
+            //return RedirectToAction("Product/AddProduct");
+
+            //return RedirectToAction("Index");
+
+            //this version we list the action first, then the controller...
+            return RedirectToAction("AddProduct", "Product");
+        }
+
+        public ActionResult GotoUpdateProduct()
+        {
+            return RedirectToAction("UpdateProduct", "Product", new { id = 20 });
+        }
+    }
+}
